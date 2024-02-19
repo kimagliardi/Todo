f3b97ca225f6068b7ed7a7612adb30db2d0b8d32
Agliardi, Kim (ESI) <kim.agliardi@gmail.com>
2024-02-19 14:22:09 -0300

    fix: test containers

--- src/test/kotlin/com/leanix/TodoRequestTest.kt
+++ src/test/kotlin/com/leanix/TodoRequestTest.kt
@@ -1,19 +1,21 @@
-package com.leanix
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
-
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @MicronautTest
+@Testcontainers
 class TodoRequestTest {
+
     companion object {
         @Container
-        val postgresContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+        val postgresContainer = PostgreSQLContainer<Nothing>("postgres:13.3").apply {
             withDatabaseName("cdc")
             withUsername("postgres")
             withPassword("postgres")
@@ -21,11 +23,11 @@ class TodoRequestTest {
 
         @JvmStatic
         @BeforeAll
-        fun beforeAll() {
-            // Certifique-se de que o container esteja iniciado
+        fun setUp() {
+            // Inicia o container
             postgresContainer.start()
 
-            // Configura o sistema ou variáveis de ambiente para utilizar a URL, usuário e senha do container
+            // Configura as propriedades do sistema com as informações do container
             System.setProperty("datasources.default.url", postgresContainer.jdbcUrl)
             System.setProperty("datasources.default.username", postgresContainer.username)
             System.setProperty("datasources.default.password", postgresContainer.password)
@@ -39,5 +41,4 @@ class TodoRequestTest {
     fun testItWorks() {
         Assertions.assertTrue(application.isRunning)
     }
-
 }
