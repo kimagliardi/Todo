9a28ffd0f388e269f06b53512a78744751614e8e
kim.agliardi@gmail.com <kim.agliardi@gmail.com>
2024-02-19 19:39:09 -0300

    fix: update operation
    Adding cascade delete & fixing subtask update

--- src/main/kotlin/com/leanix/model/Todo.kt
+++ src/main/kotlin/com/leanix/model/Todo.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference
 import io.micronaut.serde.annotation.Serdeable
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.util.*
 
 
@@ -20,5 +22,15 @@ class Todo(
     @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], orphanRemoval = true)
     @JoinColumn(name = "todo_id", referencedColumnName = "id")
     @JsonManagedReference
+    @OnDelete(action = OnDeleteAction.CASCADE)
     var tasks: MutableList<Task> = mutableListOf()
-)
+) {
+    fun replaceTasks(newTasks: List<Task>) {
+        // Clear the existing tasks
+        tasks.clear()
+        tasks.addAll(newTasks)
+
+        // Set the parent todo for each task in the new list
+        newTasks.forEach { it.todo = this }
+    }
+}
