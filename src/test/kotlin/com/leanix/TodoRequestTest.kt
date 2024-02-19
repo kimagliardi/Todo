3bdea79b3d9c33c3991afd3c2d9de285455b299a
Agliardi, Kim (ESI) <kim.agliardi@gmail.com>
2024-02-19 14:09:17 -0300

    fix: postgres test db

--- src/test/kotlin/com/leanix/TodoRequestTest.kt
+++ src/test/kotlin/com/leanix/TodoRequestTest.kt
@@ -3,10 +3,34 @@ import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+
 
 @MicronautTest
 class TodoRequestTest {
+    companion object {
+        @Container
+        val postgresContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+            withDatabaseName("cdc")
+            withUsername("postgres")
+            withPassword("postgres")
+        }
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            // Certifique-se de que o container esteja iniciado
+            postgresContainer.start()
+
+            // Configura o sistema ou variáveis de ambiente para utilizar a URL, usuário e senha do container
+            System.setProperty("datasources.default.url", postgresContainer.jdbcUrl)
+            System.setProperty("datasources.default.username", postgresContainer.username)
+            System.setProperty("datasources.default.password", postgresContainer.password)
+        }
+    }
 
     @Inject
     lateinit var application: EmbeddedApplication<*>
